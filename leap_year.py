a73b9108af826d1721a48efcfe5a3dda57e6e683
juancruzgon05 <162723271+juancruzgon05@users.noreply.github.com>
2024-04-12 12:39:20 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,6 @@
 def leap_year():
-    print("TO DO")
+    num=int(input("Ingrese un año:"))
+	if (num %4==0 and not num %100==0) or (num %100==0 and num%400==0): 
+		print(f"El año {num} es bisiesto")
+	else:
+		print(f"El año {num} no es bisiesto")
